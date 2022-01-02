f959f3827265a2cb1b455b27d5f8c35e25fcee5c
mrwick12 <akshattripathi07@gmail.com>
2022-01-02 14:19:55 +0530

    init

---
+++ Goldman Sacs/Qiestion_2.cpp
@@ -0,0 +1,8 @@
+int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
+        if(L1[0]>R2[0]||L2[0]>R1[0])//if reactangle of left side of other
+            return false;
+        if(L1[1]<R2[1]||L2[1]<R1[1])//if rctangle is above other
+            return false;
+        return true;
+    }
+};
