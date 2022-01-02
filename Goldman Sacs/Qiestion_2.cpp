70df40e157a91834759ef3a9f865c701e47d59f3
mrwick12 <akshattripathi07@gmail.com>
2022-01-02 14:45:31 +0530

    question

--- Goldman Sacs/Qiestion_2.cpp
+++ Goldman Sacs/Qiestion_2.cpp
@@ -1,3 +1,4 @@
+//question link-https://practice.geeksforgeeks.org/problems/overlapping-rectangles1924/1/
 int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
         if(L1[0]>R2[0]||L2[0]>R1[0])//if reactangle of left side of other
             return false;
