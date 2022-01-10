af5fa4001e37b24734d45bcada5ce9fa1431f7b8
mrwick12 <akshattripathi07@gmail.com>
2022-01-11 00:19:37 +0530

    init

---
+++ AMAZON/question_8.cpp
@@ -0,0 +1,6 @@
+
+link-https://practice.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter1322/1/#
+long long countWays(int m)
+    {
+        return (m/2)+1;// your code here
+    }
